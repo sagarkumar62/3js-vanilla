46990cafb07035ab2846054c7bf73fd6bb12aad4
Sagar Kumar <pandeysagar7991@gmail.com>
2025-07-01 00:20:09 +0530

    updated aspect ratio

--- src/main.js
+++ src/main.js
@@ -60,5 +60,6 @@ animate();
 window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight)
-})
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+});
